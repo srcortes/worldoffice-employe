ebe26f525f5be9ea73361ee226b661cc24762aee
= <=>
2020-10-22 15:56:53 -0500

    commit inicial

---
+++ src/test/java/com/worldoffice/test/WorldofficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worldoffice.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldofficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
